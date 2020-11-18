d5ce53c2019d32ffedca91ed434924dab29af58f
strxbae <57025819+strxbae@users.noreply.github.com>
2020-11-18 23:40:36 +0700

    Add files via upload
    update

--- phpmvc/app/controllers/About.php
+++ phpmvc/app/controllers/About.php
@@ -1,13 +1,22 @@
 <?php 
 
-class About {
-    public function index($nama = 'Fadli', $pekerjaan= 'Mahasiswa')
+class About extends Controller {
+    public function index($nama = 'Fadli', $pekerjaan= 'Mahasiswa', $umur = 19)
     {
-        echo "Halo nama Saya $nama, Saya adalah $pekerjaan";
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About Me';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
     public function page()
     {
-        echo 'About/page';
+        $data['judul'] = 'Pages';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
 ?>
--- phpmvc/app/init.php
+++ phpmvc/app/init.php
@@ -2,3 +2,5 @@
 //mamanggil semua kelas yg ada di app
 require_once 'core/App.php';
 require_once 'core/Controller.php';
+
+require_once 'core/Constants.php';
