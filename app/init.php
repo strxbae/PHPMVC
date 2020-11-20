82f12b8ca23b7922544f6719e40f3e2e13def779
Strxbae <vadlyramadhan11@gmail.com>
2020-11-20 22:00:48 +0700

    memanggil nama pada home

---
+++ app/init.php
@@ -0,0 +1,6 @@
+<?php 
+//mamanggil semua kelas yg ada di app
+require_once 'core/App.php';
+require_once 'core/Controller.php';
+
+require_once 'core/Constants.php';
---
+++ app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <h1 class="mt-3">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/my.JPG" alt="Fadli Ramadhan" width="200" class=" shadow">
+    <p>Hallo, nama saya <?= $data['nama'] ?>, umur saya <?= $data['umur'] ?> tahun, saya adalah seorang <?= $data['pekerjaan'] ?></p>
+</div>
